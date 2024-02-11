94a3c1c5a3bbe250eb59c1f4b3018875344f8bb7
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-02-10 23:57:32 -0500

    add error classes test file

---
+++ backend/src/interactor/errors/error_classes_test.py
@@ -0,0 +1,29 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+
+import pytest
+from src.interactor.errors.error_classes \
+    import FieldValueNotPermittedException
+from src.interactor.errors.error_classes import ItemNotCreatedException
+from src.interactor.errors.error_classes import UniqueViolationError
+
+def test_value_not_permitted_exception():
+    with pytest.raises(FieldValueNotPermittedException) as exception_info:
+        raise FieldValueNotPermittedException("name", "Item")
+    assert str(exception_info.value) == "Name: Item is not permitted"
+
+
+def test_item_not_created_exception():
+    with pytest.raises(ItemNotCreatedException) as exception_info:
+        raise ItemNotCreatedException("item name", "item")
+    assert str(exception_info.value) == \
+        "Item 'item name' was not created correctly"
+    
+
+def test_unique_violation_error():
+    with pytest.raises(UniqueViolationError) as exception_info:
+        raise UniqueViolationError("item", "name", "item name")
+    assert str(exception_info.value) == \
+        "Item with name: 'item name' already exists"
