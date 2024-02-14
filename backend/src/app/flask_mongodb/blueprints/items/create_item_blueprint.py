f7a7b1869cb40203b438ca7b7dd29c560d0f94d8
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-02-13 23:24:22 -0500

    Created create_item_blueprint

---
+++ backend/src/app/flask_mongodb/blueprints/items/create_item_blueprint.py
@@ -0,0 +1,22 @@
+""" Flask MongoDB Create Item Blueprint
+"""
+
+
+from flask import Blueprint, jsonify, request, current_app
+from src.app.flask_mongodb.controllers.items.create_item_controller import \ 
+    CreateItemController
+
+
+blueprint_home = Blueprint('create_item', __name__)
+
+
+@blueprint_home.route('/create_item', methods=['POST'])
+def create_item_blueprint():
+    """ Create Item Blueprint
+    """
+    logger = current_app.config['logger']
+    input_json = request.get_json(force=True)
+    controller = CreateItemController(logger)
+    controller.get_item_info(input_json)
+    result = controller.execute()
+    return jsonify(result), 201
