98086dd9745b1c879d73a03a1e9b9b1b431db0d6
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-02-13 23:39:27 -0500

    created create_item_controller_test and create_item_dtos_test

---
+++ backend/src/app/flask_mongodb/controllers/items/create_item_controller_test.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+
+import pytest
+from unittest import mock
+from src.interactor.dtos.items.create_item_dtos import CreateItemInputDto
+from src.interactor.interfaces.logger.logger import LoggerInterface
+
+
+with mock.patch()
---
+++ backend/src/interactor/dtos/items/create_item_dtos_test.py
@@ -0,0 +1,28 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+
+from .create_item_dtos import CreateItemInputDto
+
+
+def test_create_item_input_dto_valid(fixture_item_biscuit):
+    input_dto = CreateItemInputDto(
+        codebar=fixture_item_biscuit['codebar'],
+        name=fixture_item_biscuit['name'],
+        description=fixture_item_biscuit['description'],
+        price=fixture_item_biscuit['price'],
+        stock=fixture_item_biscuit['stock'],
+    )
+    assert input_dto.codebar == fixture_item_biscuit['codebar']
+    assert input_dto.name == fixture_item_biscuit['name']
+    assert input_dto.description == fixture_item_biscuit['description']
+    assert input_dto.price == fixture_item_biscuit['price']
+    assert input_dto.stock == fixture_item_biscuit['stock']
+    assert input_dto.to_dict() == {
+        "codebar": fixture_item_biscuit['codebar'],
+        "name": fixture_item_biscuit['name'],
+        "description": fixture_item_biscuit['description'],
+        "price": fixture_item_biscuit['price'],
+        "stock": fixture_item_biscuit['stock'],
+    }
