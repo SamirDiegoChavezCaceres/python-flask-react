24b2a95356c1c4cba4bb7e41c97a59a429f3b12f
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-02-13 22:07:58 -0500

    added home

--- backend/src/app/flask_mongodb/create_flask_mongodb_app.py
+++ backend/src/app/flask_mongodb/create_flask_mongodb_app.py
@@ -6,6 +6,24 @@ from flask import Flask, g
 from flask_cors import CORS
 from src.interactor.interfaces.logger.logger import LoggerInterface
 from src.infra.db_models.db_base import session, client
+from src.app.flask_mongodb.blueprints.home_blueprint import blueprint_home
+
+
+def format_error_response(
+        error: Exception, 
+        error_code: int,
+        logger: LoggerInterface
+):
+    """ Format Error Response
+    """
+    logger.log_exception(f"500 - Internal Server Error: {str(error)}")
+
+    response = {
+        'status_code': error_code,
+        'error': error.__class__.__name__,
+        'message': str(error)
+    }
+    return response, error_code
 
 
 def create_flask_mongodb_app(logger: LoggerInterface):
@@ -13,6 +31,7 @@ def create_flask_mongodb_app(logger: LoggerInterface):
     """
     app = Flask(__name__)
     app.config['logger'] = logger
+    app.register_blueprint(blueprint_home, url_prefix='/')
 
     @app.before_request
     def before_request():
